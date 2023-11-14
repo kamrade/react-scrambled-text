85f317cbdf0292888695170f64613f0fff0d8d66
Dennis <denys.mykhailov@zettahost.com>
2023-11-14 16:16:07 +0200

    Get rid of callback

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,14 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import "../index.css";
 
 export const Button = () => {
 
   const [ counter, setCounter ] = useState<number>(0);
 
-  const onClick = useCallback(() => {
+  const onClick = () => {
     setCounter(counter + 1);
     console.log("Vite + React + TypeScript + TailwindCSS", counter);
-  }, []);
+  };
 
   return (
     <button onClick={onClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
