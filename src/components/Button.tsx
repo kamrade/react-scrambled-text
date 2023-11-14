d0018db56ec603a14b469257fefc171ca9208154
Dennis <denys.mykhailov@zettahost.com>
2023-11-14 16:17:45 +0200

    Add custom css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import "../index.css";
+import "./Button.css";
 
 export const Button = () => {
 
@@ -11,7 +12,7 @@ export const Button = () => {
   };
 
   return (
-    <button onClick={onClick} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+    <button onClick={onClick} className='Button-calendar bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
       Vite is better than webpack
     </button>
   );
