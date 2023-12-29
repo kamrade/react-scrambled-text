67c532ec4aace3fa6a4139f20deb3b2815d5fd70
Dennis Michailov <denis.michailov@gmail.com>
2023-12-29 22:03:08 +0200

    Refactoring

--- src/components/ScrambledText.tsx
+++ src/components/ScrambledText.tsx
@@ -1,50 +1,42 @@
-/*
-//
-// - [ ] use declarative approach (get rid of useRef)
-//
-*/
-
-
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrambledTextProps } from "./ScrambledText.props";
 import s from './ScrambledText.module.css';
+import { getRandomChar } from './getRandomChar';
 
-export const ScrambledText: React.FC<ScrambledTextProps> = ({ value, slideLength = 2000}) => {
+export interface IQueue {
+  from: any;
+  to: any;
+  start: any;
+  end: any;
+  char?: any;
+}
 
-  const elRef = useRef<HTMLDivElement>(null);
+export const ScrambledText: React.FC<ScrambledTextProps> = ({ value, slideLength = 1000}) => {
+
   let chars = '![]#____________![]#____________';
   let resolved: any;
-  let queue: any[] = [];
+  let queue: IQueue[] = [];
   let frameRequest: number;
   let frame = 0;
   let counter = 0;
+  const [outputState, setOutputState] = useState('');
 
   // Entry point
   useEffect(() => next(), []);
 
-  // // renderer alternative
-  // const nextInterval = () => {
-  //   setInterval(() => {
-  //     setText(value[counter]);
-  //     counter = (counter + 1) % value.length;
-  //   }, slideLength)
-  // };
-
-  // renders each phrase in a loop
   const next = () => {
-    setText(value[counter]).then(() => setTimeout(next, slideLength));
+    let prevText = value[ counter === 0 ? value.length - 1 : counter - 1 ];
+    setText( value[counter], prevText ).then(() => setTimeout(next, slideLength));
     counter = (counter + 1) % value.length;
   }
 
-  // set text
-  const setText = (newText: string) => {
-    let content   = (elRef.current && elRef.current.innerText) || '';          // получить текущий отображенный текст
-    const length  = Math.max(content.length, newText.length);                  // установить максимальную длину для currentText и newText
-    const promise = new Promise((resolve, _reject) => resolved = resolve);    // показывает resolved наружу
-    queue = []; // сброс очереди символов
+  const setText = (newText: string, prevText: string) => {
+    const length = Math.max(prevText.length, newText.length);
+    const promise = new Promise((resolve, _reject) => resolved = resolve);
+    queue = [];
 
     for (let i = 0; i < length; i++) {
-      const from   = content[i] || '';
+      const from   = prevText[i] || '';
       const to     = newText[i] || '';
       const start  = Math.floor(Math.random() * 20);          // start и end – это рандомный промежуток
       const end    = start + Math.floor(Math.random() * 20);  // между 0 и 80, например [0,2], [10,25], [40, 80]
@@ -57,56 +49,42 @@ export const ScrambledText: React.FC<ScrambledTextProps> = ({ value, slideLength
     return promise;
   }
 
-  // отрисовка фрейма (кадра) на базе queue
-  // каждый вызов пробегается по queue и, при необходимости, добавляет char в queue[i]
-  // при этом остальные поля остаются статичными
   const update = () => {
-    let output = '';  // базовое значение текста
-    let complete = 0; // счетчик. максимальное значение = queue.length
-
-    // в этом цикле генерируется строка, которая будет отображаться в текущем фрейме.
-    // в зависимости от того, длина (в символах) какого фрейма больше, текст "вырастает" или "уменьшается"
+    setOutputState('');
+    let complete = 0;
     for (let i = 0, n = queue.length; i < n; i++) {
       let { from, to, start, end, char } = queue[i];
 
       if (frame >= end) {
         complete++;
-        output += to;
+        setOutputState((prevState) => prevState + to);
       } else if (frame >= start) {
         if (!char || Math.random() < 0.28) {
-          char = randomChar();
+          char = getRandomChar(chars);
           queue[i].char = char;
         }
-        // TODO: move class or styles to component scope. Now you can find class definition in the addons.scss
-        output += `<span class="dud">${char}</span>`;
-        // output += `${char}`;
+        setOutputState((prevState) => prevState + char);
 
       } else {
-        output += from;
+        setOutputState((prevState) => prevState + from);
       }
     }
 
-
-    // положить новый текст в DOM
-    if (elRef.current) {
-      elRef.current.innerHTML = output;
-    }
-
     // Если прошлись по всему queue, то пора заканчивать если нет, то идем дальше.
-    complete === queue.length ? resolved()
-      : frameRequest = requestAnimationFrame(update);
-        frame++;
-
-  }
-
-  const randomChar = () => {
-    return chars[Math.floor(Math.random() * chars.length)];
+    if (complete === queue.length) {
+      resolved();
+    } else {
+      frameRequest = requestAnimationFrame(update);
+    }
+    frame++;
   }
 
   return (
     <div className={s.ScrambledTextContainer}>
-      <div ref={elRef} className={s.ScrambledText}>
-
+      <div className={s.ScrambledText}>
+        {outputState.split('').map((item, i) => (
+          <span className="togo" key={i}>{item}</span>
+        ))}
       </div>
     </div>
   );
---
+++ src/components/getRandomChar.ts
@@ -0,0 +1,2 @@
+export const getRandomChar = (chars: string) => 
+    chars[Math.floor(Math.random() * chars.length)];
